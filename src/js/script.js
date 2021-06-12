56f6dd986fb624a9490753e6d531b700b6a48472
Zach Chun <chunz2@cs.washington.edu>
2021-06-12 16:17:41 -0700

    Add pink gradient timing boxes

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,26 @@ let context;
 window.onload = init;
 
 function draw() {
-  let columnFraction = (1 / 8.0);
+  let columnFraction = (1 / 10.0);
   console.log('draw');
   context.fillStyle = '#000';
+
+  // columns
   for (let i = 0; i < 7; i++) {
     let columnWidth = canvas.width * columnFraction;
-    context.fillRect((columnWidth / 2.0) + columnWidth * i, 0, columnWidth, canvas.height);
+    context.fillRect(1.5 * columnWidth + columnWidth * i, 0, columnWidth - 1, (2.0 / 3.0) * canvas.height);
+  }
+
+  // hit timing boxes
+  for (let i = 0; i < 7; i++) {
+    let columnWidth = canvas.width * columnFraction;
+    var grd = context.createLinearGradient(0,(2.0 / 3.0) * canvas.height, 0 ,(2.0 / 3.0) * canvas.height + (2 * (columnWidth - 1)));
+    grd.addColorStop(0,"#d011c8");
+    grd.addColorStop(1,"black");
+
+    // Fill with gradient
+    context.fillStyle = grd;
+    context.fillRect(1.5 * columnWidth + columnWidth * i, (2.0 / 3.0) * canvas.height, columnWidth - 1, (columnWidth - 1));
   }
 }
 
