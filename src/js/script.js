b2c605a7fde17c355f0a59944d29d975d1ce57a4
Zach Chun <chunz2@cs.washington.edu>
2021-06-12 15:36:13 -0700

    Recreate note scrolling window with Canvas

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,31 @@
-console.log('hello');
+"use strict";
+let canvas;
+let context;
+window.onload = init;
 
-const sound = new Audio();
-const button = document.querySelector('button');
-button.addEventListener('click', playSound);
+function draw() {
+  let columnFraction = (1 / 8.0);
+  console.log('draw');
+  context.fillStyle = '#000';
+  for (let i = 0; i < 7; i++) {
+    let columnWidth = canvas.width * columnFraction;
+    context.fillRect((columnWidth / 2.0) + columnWidth * i, 0, columnWidth, canvas.height);
+  }
+}
+
+function init() {
+  // Get a reference to the canvas
+  canvas = document.getElementById('canvas');
+  context = canvas.getContext('2d');
+  draw();
+
+  const button = document.querySelector('button');
+  button.addEventListener('click', playSound);
+}
+
 
 function playSound() {
+  const sound = new Audio();
   sound.src = 'mp3s/example.mp3';
   sound.play();
 }
